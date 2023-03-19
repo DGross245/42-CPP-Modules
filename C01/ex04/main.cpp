0e8b76094cfae701b3611b23f038ad6126125c71
DGross245 <103501922+DGross245@users.noreply.github.com>
2023-03-19 13:50:59 +0100

    added ex04

--- C01/ex04/main.cpp
+++ C01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dgross <dgross@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 17:15:49 by dgross            #+#    #+#             */
-/*   Updated: 2023/02/22 09:25:43 by dgross           ###   ########.fr       */
+/*   Updated: 2023/03/19 13:46:54 by dgross           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,30 @@
 #include <iostream>
 #include <fstream>
 
-void replace (char *filename, char *s1, char *s2) {
-	std::fstream inputFile(filename);
-	std::string	line;
 
+void ReadFile (std::string filename, std::string s1, std::string s2) {
+	std::fstream inputFile;
+	std::fstream outFile;
+	std::string	line;
+	std::size_t position;
+
+	inputFile.open(filename, std::fstream::in);
+	outFile.open(filename + ".replace", std::fstream::out);
 	if (inputFile.is_open())
 	{
 		while(getline( inputFile, line))
 		{
-			
+			while (line.find(s1) != std::string::npos)
+			{
+				position = 0;
+				while ((position = line.find(s1)) != std::string::npos)
+				{
+					line.erase(position, s1.length());
+					line.insert(position, s2);
+					position += s2.length();
+				}
+				outFile << line << std::endl;
+			}
 		}
 		inputFile.close();
 	}
@@ -38,6 +53,6 @@ int main( int argc, char **argv )
 		std::cout << "Wrong number of arg" << std::endl;
 		return (1);
 	}
-	replace(argv[1], argv[2], argv[3]);
+	ReadFile(argv[1], argv[2], argv[3]);
 	return (0);
 }
