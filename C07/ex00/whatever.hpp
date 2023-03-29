51b22367dd195acb7697e974a0e341d9fcb9134b
DGross245 <103501922+DGross245@users.noreply.github.com>
2023-03-29 17:55:50 +0200

    commit

--- C07/ex00/whatever.hpp
+++ C07/ex00/whatever.hpp
@@ -6,28 +6,37 @@
 /*   By: dgross <dgross@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/25 21:28:55 by dna               #+#    #+#             */
-/*   Updated: 2023/03/27 13:36:16 by dgross           ###   ########.fr       */
+/*   Updated: 2023/03/29 14:56:15 by dgross           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+
 #ifndef WHATEVER_H
 
 # define WHATEVER_H
 
 template<typename whatever>
-void swap(){
-	
+void swap(whatever &a, whatever &b) {
+	whatever c;
+
+	c = a;
+	a = b;
+	b = c;
 }
 
 template<typename whatever>
-void min(){
-	
-}
-template<typename whatever>
-void max(){
-	
+whatever min(whatever a, whatever b) {
+	if (a < b)
+		return (a);
+	return (b);
 }
 
+template<typename whatever>
+whatever max(whatever a, whatever b) {
+	if (a > b)
+		return (a);
+	return (b);
+}
 
 #endif
---
+++ C07/ex01/iter.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dgross <dgross@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/29 14:55:38 by dgross            #+#    #+#             */
+/*   Updated: 2023/03/29 15:55:19 by dgross           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#ifndef ITER_H
+
+# define ITER_H
+
+#include <string>
+
+template<typename inter>
+void Inter(std::string &a, std::string b, inter c) {
+	
+}
+#endif
