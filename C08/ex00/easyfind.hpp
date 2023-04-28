fe5879bc0e346352d0ce17e1e793ae9d21c2d821
DGross245 <103501922+DGross245@users.noreply.github.com>
2023-04-28 17:17:41 +0200

    fixed compile error

--- C08/ex00/easyfind.hpp
+++ C08/ex00/easyfind.hpp
@@ -6,16 +6,17 @@
 /*   By: dna <dna@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/16 13:09:51 by dna               #+#    #+#             */
-/*   Updated: 2023/04/17 01:47:25 by dna              ###   ########.fr       */
+/*   Updated: 2023/04/28 17:15:15 by dna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_H
 # define EASYFIND_H
 # include <iostream>
+#include <algorithm>
 
 template<typename T>
-int easyfind( T const &container , int searchValue ) {
+int easyfind( T const &container , const int searchValue ) {
 	typename T::const_iterator Interator;
 
 	Interator = std::find(container.begin(), container.end(), searchValue);
